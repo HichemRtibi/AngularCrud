0c8a523f0a54d92bfbd332226c6096cd1de3260b
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-30 06:39:07 +0100

    create list-employee component et model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LisEmployeesComponent } from './employees/lis-employees/lis-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LisEmployeesComponent
   ],
   imports: [
     BrowserModule,
