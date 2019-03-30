21be4ef6121a21c668a72b5419ec8bce2415c20e
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-30 14:46:59 +0100

    installationngx-bootstrap and config bsDatepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
-import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EmployeeListComponent} from './employees/employee-list/employee-list.component';
+import {CreateEmployeeComponent} from './employees/create-employee/create-employee.component';
 import {FormsModule} from '@angular/forms';
+import {BsDatepickerModule} from 'ngx-bootstrap';
 
 
 @NgModule({
@@ -18,9 +19,11 @@ import {FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BsDatepickerModule.forRoot(),
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
